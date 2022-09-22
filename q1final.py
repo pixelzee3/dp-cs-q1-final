d3bbac7bc85d7c357a1fa2cb19184aee797b7176
PixelZee3 <bpixelzee@gmail.com>
2022-09-22 16:08:36 +0700

    Add guiding comments.

---
+++ q1final.py
@@ -0,0 +1,3 @@
+# Potential IOT device: Smart bike
+# Device description: In terms of hardware, the smart bike is able to electronically adjust the seat and handlebar heights. The bike is also has a display to display the user's name and reminders for checking the tire pressures. Besides that, the smart bike is able to analyze the user's cycling performance using a collection of data.
+# Device personality: Informative
